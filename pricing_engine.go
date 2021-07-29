11d6ce48054db1dff134b6ce22327fd3958065d6
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-07-29 16:43:00 +0530

    Updating the request/response pojo with required fields

--- pricing_engine.go
+++ pricing_engine.go
@@ -3,10 +3,25 @@ package pricingengine
 // GeneratePricingRequest is used for generate pricing requests, it holds the
 // inputs that are used to provide pricing for a given user.
 type GeneratePricingRequest struct {
-	// TODO: populate me!
+  DateOfBirth string `json:"date_of_birth"`
+  InsuranceGroup string `json:"insurance_group"`
+  LicenseHeldSince string `json:"license_held_since"`
 }
 
-// GeneratePricingResponse - TODO: please document me :)
+// GeneratePricingResponse - contains the list of all pricing generated for the request passed
+// it typically has the input based on which the decision is taken
+// IsEligible to indicate whether the user is eligible
+// Message to state the reason for thich the Decline has happened
 type GeneratePricingResponse struct {
-	// TODO: populate me!
+	input GeneratePricingRequest
+  IsEligible string `json:"is_eligible"`
+  Message string `json:"message"`
+  pricing_list []PricingItem `json:"pricing"`
+}
+
+// PricingItem - contains the pricing data generated for partucular group based on the request passed
+type PricingItem struct {
+	Premium float64 `json:"premium"`
+  Currency string  `json:"premium"`
+  FareGroup string `json:"fare_group"`
 }
