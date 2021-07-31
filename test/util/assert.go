207a2761326f260c5377c9d781b8d945526d908a
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-07-31 15:17:41 +0530

    Refactor assert equal class for optimal checking + tests

--- test/util/assert.go
+++ test/util/assert.go
@@ -1,13 +1,20 @@
 package util
 
 import (
+  "log"
   "testing"
   "reflect"
 )
 
 
+func TestAssert(t *testing.T) {
+  log.Println("Some testing here!")
+  AssertEqual(1, 1, t)
+  AssertEqual([1]int{1}, [1]int{1}, t)
+}
+
 func AssertEqual(a interface{}, b interface{}, t *testing.T) {
-  if a == b || reflect.DeepEqual(a, b) {
+  if reflect.DeepEqual(a, b) {
     return
   }
 	t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
