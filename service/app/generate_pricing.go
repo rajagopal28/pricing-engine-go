e67e8de6a3a0fb27ddc32c3a6f6299eb216d56a8
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2021-08-01 15:18:51 +0530

    Making service plugable for testing with test config paths

--- service/app/generate_pricing.go
+++ service/app/generate_pricing.go
@@ -17,16 +17,37 @@ type App struct{
 // be denied.
 func (a *App) GeneratePricing(ctx context.Context, request *pricingengine.GeneratePricingRequest) (*pricingengine.GeneratePricingResponse, error) {
 	log.Println("Entering GeneratePricing")
-	a.Cache.Fetcher = config.ConfigFetcher{Path: "/config/"} // Initialise with actual path
+	// Initialise with actual path if not present
+	if a.Cache.TimeToLive == 0 {
+		a.Cache.Fetcher = config.ConfigFetcher{Path: "/config/"}
+	}
 	a.Cache.InitialiseWithRefresh(false, 100000) // time to re-Initialise and see if the cache works
 
 	result := pricingengine.GeneratePricingResponse{}
 	result.Input = *request
 
+	if len(request.DateOfBirth) == 0 {
+		result.Message = "DateOfBirth cannot be empty"
+		result.IsEligible = false
+		return &result, nil
+	}
+
+	if request.InsuranceGroup <= 0 {
+		result.Message = "InsuranceGroup should be a Positive number"
+		result.IsEligible = false
+		return &result, nil
+	}
+
+	if len(request.LicenseHeldSince) == 0 {
+		result.Message = "LicenseHeldSince Date cannot be empty"
+		result.IsEligible = false
+		return &result, nil
+	}
+
 	var strategies = Strategy{}
 	driver_factor_range, err := strategies.FindMatchingDriverAgeFactor(request, a.Cache.DriverAgeFactorList)
 	if(err != nil) {
-		log.Fatalf("error finding driver_factor_range: %v", err)
+		log.Printf("error finding driver_factor_range: %v", err)
 		result.Message = err.Error()
 		result.IsEligible = false
 		return &result, nil
@@ -34,7 +55,7 @@ func (a *App) GeneratePricing(ctx context.Context, request *pricingengine.Genera
 
 	insurance_factor_range, err := strategies.FindMatchingInsuranceGroupFactor(request, a.Cache.InsuranceGroupFactorList)
 	if(err != nil) {
-		log.Fatalf("error finding insurance_factor_range: %v", err)
+		log.Printf("error finding insurance_factor_range: %v", err)
 		result.Message = err.Error()
 		result.IsEligible = false
 		return &result, nil
@@ -42,7 +63,7 @@ func (a *App) GeneratePricing(ctx context.Context, request *pricingengine.Genera
 
 	licence_factor_range, err := strategies.FindMatchingLicenceValidityFactor(request, a.Cache.LicenceValidityFactorList)
 	if(err != nil) {
-		log.Fatalf("error finding licence_factor_range: %v", err)
+		log.Printf("error finding licence_factor_range: %v", err)
 		result.Message = err.Error()
 		result.IsEligible = false
 		return &result, nil
@@ -67,7 +88,7 @@ func (a *App) GeneratePricing(ctx context.Context, request *pricingengine.Genera
 	for i:= 0; i < len(a.Cache.BaseRateList); i++ {
 			item, err := strategies.ApplyBasePricing(request, &a.Cache.BaseRateList[i], firstStrategy)
 			if(err != nil) {
-				log.Fatalf("error finding ApplyBasePricing: %v", err)
+				log.Printf("error finding ApplyBasePricing: %v", err)
 				return &result, err
 			}
 			price_items = append(price_items, *item)
